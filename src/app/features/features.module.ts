af676d173942707acfd855685a4837d2902fe149
Dinithi-prog <kgalmadushanka@gmail.com>
2024-06-09 21:17:21 +0530

    remove attendance dir

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,8 +1,7 @@
 import {Module} from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
-  imports: [UserModule, AttendanceModule],
+  imports: [UserModule],
 })
 export class FeaturesModule {}
