936187080c05479a990dcbcafb0c49d6b6680823
Dinithi-prog <kgalmadushanka@gmail.com>
2024-06-09 19:42:10 +0530

    Attendance added

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,7 +1,8 @@
 import {Module} from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AttendanceModule],
 })
 export class FeaturesModule {}
